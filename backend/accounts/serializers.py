90c0226a9414af392608c0fab3262b2bdd2e4ee3
Aryan Shrestha <68954986+aryan-shrestha@users.noreply.github.com>
2021-11-29 21:49:26 +0545

    first-commit

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,10 @@
+from djoser.conf import User
+from djoser.serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'email', 'name', 'password')
